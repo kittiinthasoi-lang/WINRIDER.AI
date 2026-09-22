ed309d3f3877426eee23df9b77f87aada3f88e39
kittiinthasoi-lang <kittiinthasoi@gmail.com>
2026-09-22 15:02:22 +0700

    Fix PWA caching for bundled image assets

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig(() => {
           ],
         },
         workbox: {
-          globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+          globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,webp,gif,svg,woff,woff2}'],
           maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         },
         devOptions: {
